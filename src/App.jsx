9ebcab739e35d3262a2da49767325ec9fcdc1e83
SuyogLuitel <suyogluitel235@gmail.com>
2025-04-18 11:05:30 +0545

    fix: hashrouter added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter,HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Archive from "./pages/Archive";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/archive" element={<Archive />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
